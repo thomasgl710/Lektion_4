8afd2f1761d2ea17aa94af0afe768a1d3b33b58f
thomasgl710 <thomasgl10@gmail.com>
2020-09-24 13:03:37 +0200

    Projekt oprettet

---
+++ Door/Door/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Door
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
